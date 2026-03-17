18b91d030c038ca4bc0aa949fefc4a332a61c3c3
pinky <pinky.k0201@gmail.com>
2026-03-17 12:08:41 +0530

    done

---
+++ oper.py
@@ -0,0 +1,16 @@
+set_a = {1, 2, 3, 4}
+set_b = {3, 4, 5, 6}
+
+# Union
+union_set = set_a | set_b
+
+# Intersection
+intersection_set = set_a & set_b
+
+# Difference (elements in set_a but not in set_b)
+difference_set = set_a - set_b
+
+# Print results
+print("Union:", union_set)
+print("Intersection:", intersection_set)
+print("Difference (set_a - set_b):", difference_set)
