a15f064c799114562008e347bdd9379842920ac5
pinky <pinky.k0201@gmail.com>
2026-03-24 17:54:15 +0530

    done

---
+++ string6.py
@@ -0,0 +1,16 @@
+text = "apple,Banana,grapes"
+print(text.split())
+
+words = "python","is","awesome"
+print(' '.join(words))
+
+multiline_string = '''This is
+a multiline
+string.'''
+
+print(multiline_string)
+res=multiline_string
+print(res)
+
+for i in res:
+    print(i)
