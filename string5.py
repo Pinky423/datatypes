5d3d0651e9b352477fea9c24e57866a776313b81
pinky <pinky.k0201@gmail.com>
2026-03-24 17:31:51 +0530

    done

---
+++ string5.py
@@ -0,0 +1,7 @@
+text = "machine learning and AI are trending"
+
+print(text.find("AI"))
+print(text.replace("AI","Artificial Intelligence"))
+
+data = "data data mining and big data"
+print(data.count("data"))
