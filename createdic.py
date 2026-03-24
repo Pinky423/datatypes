04401bf60b8ceebab0955a7ce50d87c1349981b6
pinky <pinky.k0201@gmail.com>
2026-03-24 16:55:40 +0530

    done

--- createdic.py
+++
@@ -1,6 +0,0 @@
-keys = ['id', 'name', 'email']
-values = [101, 'Bob', 'bob@example.com']
-
-my_dict = dict(zip(keys, values))
-
-print(my_dict)
--- del.py
+++
@@ -1,6 +0,0 @@
-
-my_list = [10, 20, 30, 40, 50]
-
-del my_list[2]
-
-print(my_list)
