e19ca0c4c102eadada9ad357840c140db87b1b26
pinky <pinky.k0201@gmail.com>
2026-03-24 17:18:30 +0530

    done

---
+++ string3.py
@@ -0,0 +1,9 @@
+text = "naman"
+
+reversed_text = text[::-1]
+print("Reversed string:", reversed_text)
+
+if text == reversed_text:
+    print("It is a palindrome.")
+else:
+    print("It is not a palindrome.")
