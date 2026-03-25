8bfbee9e64ce316fde9c828ca1091e930a72bcb2
pinky <pinky.k0201@gmail.com>
2026-03-25 17:15:03 +0530

    done

---
+++ list1.py
@@ -0,0 +1,14 @@
+fruits = ["Apple", "Banana", "Orange", "Grapes", "Pineapple"]
+
+print("Second:", fruits[1])
+print("Last:", fruits[-1])
+
+fruits.append("Mango")
+
+fruits.pop(0)
+
+fruits.sort()
+
+fruits.reverse()
+
+print(fruits)
