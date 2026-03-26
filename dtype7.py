640f42cb8b8607bfcf8779f81fc0ddf980a254f4
pinky <pinky.k0201@gmail.com>
2026-03-26 16:44:17 +0530

    done

---
+++ dtype7.py
@@ -0,0 +1,14 @@
+num = int("123")
+print("String to int:", num)
+
+lst = [1, 2, 3]
+tup = tuple(lst)
+print("List to tuple:", tup)
+
+t = (4, 5, 6)
+lst2 = list(t)
+print("Tuple to list:", lst2)
+
+pairs = [(1, 'A'), (2, 'B')]
+d = dict(pairs)
+print("Pairs to dict:", d)
