26845e0c00255b22d6a2099860b60eafafe98864
pinky <pinky.k0201@gmail.com>
2026-04-07 20:26:12 +0530

    done

--- recursion16.py
+++ recursion16.py
@@ -1,15 +1,8 @@
-def split_string(s):
-    vowels = ""
-    others = ""
-    
-    for ch in s:
-        if ch.lower() in "aeiou":
-            vowels += ch
-        else:
-            others += ch
-    
-    return vowels, others
+def calculate(lst):
+    return sum(lst),max(lst),min(lst)
 
-v, o = split_string("hello")
-print("Vowels:", v)
-print("Others:", o)
+nums=[10,40,30]
+s,mx,mn=calculate(nums)
+print("sum:",s)
+print("Max:",mx)
+print("Min:",mn)
