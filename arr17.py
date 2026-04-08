4dfe816278a4ee034d84c1e76af95cca172ace55
pinky <pinky.k0201@gmail.com>
2026-04-08 17:01:45 +0530

    done

---
+++ arr17.py
@@ -0,0 +1,9 @@
+arr = [10, 20, 30, 40, 50, 60, 70]
+
+print("First 5 elements:")
+for i in range(5):
+    print(arr[i])
+
+print("Alternate elements:")
+for i in range(0, len(arr), 2):
+    print(arr[i])
