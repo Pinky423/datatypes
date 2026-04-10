76da8c2d4db802a0153ae3c1c39aee465b327fb7
pinky <pinky.k0201@gmail.com>
2026-04-10 17:21:44 +0530

    done

---
+++ oop3.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self):
+        self.__title=""
+        self.__author=""
+    def set_data(self,t,a):
+        self.__title=t
+        self.__author=a
+    def get_data(self):
+        print("Title:",self.__title)
+        print("Author:",self.__author)
+b=Book()
+b.set_data("djfs","dwurt")
+b.get_data()
