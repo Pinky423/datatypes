820a7a9f85904c0dbac767e3d83f3a16aad6cdd9
pinky <pinky.k0201@gmail.com>
2026-04-10 17:48:24 +0530

    done

---
+++ oop5.py
@@ -0,0 +1,16 @@
+class Account:
+    def __init__(self):
+        self.__balance=0
+    def deposit(self,amt):
+        self.__balance+=amt
+    def withdraw(self,amt):
+        if amt<=self.__balance:
+            self.__balance-=amt
+        else:
+            print("Sufficient amount")
+    def show(self):
+        print("Amount:",self.__balance)
+a=Account()
+a.deposit(4000)
+a.withdraw(1800)
+a.show()
