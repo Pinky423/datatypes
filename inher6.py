b40ff3348e316aee48651aeb434944b91151293c
pinky <pinky.k0201@gmail.com>
2026-04-21 16:48:34 +0530

    done

--- inher6.py
+++ inher6.py
@@ -1,21 +1,5 @@
-class A:
-    def show(self):
-        print("Class A")
+a = 10
+b = "Hello"
 
-class B(A):
-    def show(self):
-        print("Class B")
-        super().show()
-
-class C(A):
-    def show(self):
-        print("Class C")
-        super().show()
-
-class D(B, C):
-    def show(self):
-        print("Class D")
-        super().show()
-
-d = D()
-d.show()
+print(type(a))
+print(type(b))
