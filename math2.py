38a1d12da9fbde744f13447e828e68383b21aa20
pinky <pinky.k0201@gmail.com>
2026-05-08 17:49:11 +0530

    done

---
+++ math2.py
@@ -0,0 +1,11 @@
+import math
+
+r = float(input("Enter radius: "))
+
+area = math.pi * r * r
+
+print("Area of Circle:", area)
+
+num = float(input("Enter number for log: "))
+
+print("Natural Log:", math.log(num))
